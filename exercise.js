34e035dbed0bfd0d0585017077581babfb0bf21c
Nguyen Cong Duy <ncongduy@gmail.com>
2020-10-23 21:35:27 +0300

    create timer and add audio

---
+++ exercise.js
@@ -0,0 +1,52 @@
+// // Set the date we're counting down to
+// var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
+
+// // Update the count down every 1 second
+// var x = setInterval(function () {
+//   // Get today's date and time
+//   var now = new Date().getTime();
+
+//   // Find the distance between now and the count down date
+//   var distance = countDownDate - now;
+
+//   // Time calculations for days, hours, minutes and seconds
+//   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+//   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+//   // Output the result in an element with id="demo"
+//   document.getElementById("demo").innerHTML =
+//     days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+//   // If the count down is over, write some text
+//   if (distance < 0) {
+//     clearInterval(x);
+//     document.getElementById("demo").innerHTML = "EXPIRED";
+//   }
+// }, 1000);
+
+//Setup Timer
+const current = new Date().getTime();
+
+const timer = setInterval(function () {
+  let now = new Date().getTime();
+  let distance = now - current;
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("timer").innerHTML = minutes + "m " + seconds + "s ";
+}, 1000);
+
+//Setup announcement
+const start = document.querySelector(".button");
+
+function playAudio(key) {
+  const audio = document.getElementById(key.dataset.note);
+  audio.currentTime = 0;
+  audio.play();
+}
+
+start.onclick = () => {
+  playAudio(start);
+};
